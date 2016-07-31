372ee757f7b986045843b087062a2348bc8c5313
Andrews, John L <John.L.Andrews@disney.com>
2016-07-31 16:41:45 -0700

    create adapter for recycle view

--- app/src/main/java/Adapter/StoryAdapter.java
+++ app/src/main/java/Adapter/StoryAdapter.java
@@ -1,39 +1,62 @@
 package Adapter;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import Model.Article;
 import codepath.articlesearch.R;
 
 /**
  * Created by andrj148 on 7/31/16.
  */
 public class StoryAdapter extends RecyclerView.Adapter<StoryAdapter.ViewHolder> {
+    private Article[] articles;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private View view;
+        private CardView cardView;
 
-        public ViewHolder(View v) {
+        public ViewHolder(CardView v) {
             super(v);
-            view = v;
+            cardView = v;
         }
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_article_result, parent, false);
-        return new ViewHolder(view);
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(R.layout.card_story, parent, false);
+        return new ViewHolder(cv);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        Article givenArticle = articles[position];
+        CardView cardView = holder.cardView;
 
+        ImageView imageView = (ImageView) cardView.findViewById(R.id.ivStoryImage);
+        imageView.setImageResource(0);
+        String thumbnail = givenArticle.getThumbnail();
+        if (!TextUtils.isEmpty(thumbnail)) {
+            Picasso.with(cardView.getContext()).load(thumbnail).into(imageView);
+        }
+
+        TextView textView = (TextView) cardView.findViewById(R.id.tvStoryHeadline);
+        String headline = givenArticle.getHeadline() + System.getProperty("line.separator") + givenArticle.getDate();
+        textView.setText(headline);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.length;
+    }
+
+    public StoryAdapter(Article[] articleSearchResults) {
+        this.articles = articleSearchResults;
     }
 }
